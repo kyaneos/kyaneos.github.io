5167be91cd5e9a34dc84f322d7e73484e3fae83f
Justin Moran <kyaneos@mac.lan>
2025-09-17 19:41:47 -0700

    Fix favicon path in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,7 +19,7 @@
 <svelte:head>
 	<title>Justin Moran</title>
 	<meta name="description" content="Justin Moran - Cognitive Science PhD Student at UC San Diego" />
-	<link rel="icon" href="/src/lib/assets/favicon.ico" />
+	<link rel="icon" href="/favicon.ico" />
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
 </svelte:head>
 
